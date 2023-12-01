91f8e93b1c665b176db38c85a55f2e97d13580b1
Moises-Barbosa <moises-_-barbosa@outlook.com>
2023-12-01 09:51:38 -0300

    Melhoria no exemplo 05

--- exem05.py
+++ exem05.py
@@ -11,3 +11,16 @@ for i in range(6):
 
 l3 = (soma_sem_repeticao(l1, l2))
 print(l3)
+print()
+
+#Melhoria no código
+def soma_sem_repeticao(l1, l2):
+  return(set(l1 + l2))
+
+l1 = list(range(6))
+l2 = list(range(3, 9))
+l3 = []
+
+l3 = (soma_sem_repeticao(l1, l2))
+print(l3)
+
