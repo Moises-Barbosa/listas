7b8d69ea68bb7258ef24d303612c1e9e1129813b
Moises-Barbosa <moises-_-barbosa@outlook.com>
2023-12-01 09:33:28 -0300

    Exemplo 04

---
+++ exem04.py
@@ -0,0 +1,29 @@
+#1º forma
+def soma(l1, l2):
+  return l1 + l2
+
+l1 = [1, 2, 3, 4, 5]
+l2 = [6, 7, 8, 9, 10]
+print(soma(l1, l2))
+print()
+
+#2º forma
+l1 = [1, 2, 3, 4, 5]
+l2 = [6, 7, 8, 9, 10]
+l3 = []
+
+l3.extend(l1)
+l3.extend(l2)
+print(l3)
+print()
+
+#3º forma
+l1 = []
+l2 = []
+
+for i in range(5):
+  l1.append(i+1)
+for i in range(5):
+  l2.append(i+6)
+
+print(soma(l1, l2))
